d936ab6ff3a010b0d071bbba407ad0e0323bff04
Tomas Vega <tomasvegagalvez@gmail.com>
2015-02-10 00:49:04 -0800

    added ir and ms

---
+++ oscar_motion_sensor/oscar_motion_sensor.ino
@@ -0,0 +1,64 @@
+/* 
+ * //////////////////////////////////////////////////
+ * //making sense of the Parallax PIR sensor's output
+ * //////////////////////////////////////////////////
+ *
+ * Switches a LED according to the state of the sensors output pin.
+ * Determines the beginning and end of continuous motion sequences.
+ *
+ * @author: Kristian Gohlke / krigoo (_) gmail (_) com / http://krx.at
+ * @date:   3. September 2006 
+ *
+ * kr1 (cleft) 2006 
+ * released under a creative commons "Attribution-NonCommercial-ShareAlike 2.0" license
+ * http://creativecommons.org/licenses/by-nc-sa/2.0/de/
+ *
+ *
+ * The Parallax PIR Sensor is an easy to use digital infrared motion sensor module. 
+ * (http://www.parallax.com/detail.asp?product_id=555-28027)
+ *
+ * The sensor's output pin goes to HIGH if motion is present.
+ * However, even if motion is present it goes to LOW from time to time, 
+ * which might give the impression no motion is present. 
+ * This program deals with this issue by ignoring LOW-phases shorter than a given time, 
+ * assuming continuous motion is present during these phases.
+ *  
+ */
+
+/////////////////////////////
+//VARS
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;        
+int pirPin = 2;    //the digital pin connected to the PIR sensor's output
+
+
+/////////////////////////////
+//SETUP
+void setup(){
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+  }
+
+////////////////////////////
+//LOOP
+void loop(){
+  if(digitalRead(pirPin) == HIGH)
+  {
+    Serial.println("Motion detected.");
+  } 
+  else
+  {
+    Serial.println("Chilling.");
+  }
+}
