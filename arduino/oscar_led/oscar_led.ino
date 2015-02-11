8395b3d5e3444d73c112dc0fc5bb8086dacaa6f8
Tomas Vega <tomasvegagalvez@gmail.com>
2015-02-11 15:50:00 -0800

    works!

--- arduino/oscar_led/oscar_led.ino
+++ arduino/oscar_led/oscar_led.ino
@@ -1,4 +1,5 @@
 #include <Wtv020sd16p.h>
+#include <DistanceGP2Y0A21YK.h>
 
 
 int resetPin = 2;  // The pin number of the reset pin.
@@ -15,17 +16,27 @@ int green = 12;
 int yellow = 11;
 int red = 10;
 
-int blue_id = 3;
-int green_id = 2;
-int yellow_id = 1;
-int red_id = 0;
+int blueId = 3;
+int greenId = 2;
+int yellowId = 1;
+int redId = 0;
+
+//SENSORS//
+//Distance
+DistanceGP2Y0A21YK Dist;
+int infraRed = A0;
+int distance;
+int maxDistance = 80;
+
+//Motion
+int calibrationTime = 30;   
+int motionSensor = 9;
 
 int state = 1;
 // 0 = default (idle)
 // 1 = measure trash level
 
-boolean motion = false;
-int trash_level = 0;
+int trashLevel = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -35,51 +46,80 @@ void setup() {
   pinMode(yellow, OUTPUT);     
   pinMode(red, OUTPUT);     
   wtv020sd16p.reset();
+  
+  Serial.begin(9600);
+  Dist.begin(A0);
+  pinMode(motionSensor, INPUT);
+  digitalWrite(motionSensor, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+  for(int i = 0; i < calibrationTime; i++){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+  delay(50);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // TODO: set state = 1 if detecting motion.
-  state = 1; // later remove this line, added by Sumer to default state to 1 for audio testing
+  state = digitalRead(motionSensor); // later remove this line, added by Sumer to default state to 1 for audio testing
   if(state==1) {
+    Serial.println("motion detected");
     // Change random() to a call to IR sensor function
     // The function should return
     // - 0 for full
     // - 1 for almost full
     // - 2 for half
     // - 3 for empty
-    trash_level = random(0, 4);
+    trashLevel = getTrashLevel();
+    Serial.print("trash level: ");
+    Serial.println(trashLevel);
 
-    if(trash_level<=blue_id) {
+    if(trashLevel<=blueId) {
       digitalWrite(blue, HIGH);
     }
-    if(trash_level<=green_id) {
+    if(trashLevel<=greenId) {
       digitalWrite(green, HIGH);
     }
-    if(trash_level<=yellow_id) {
+    if(trashLevel<=yellowId) {
       digitalWrite(yellow, HIGH);
     }
-    if(trash_level<=red_id) {
+    if(trashLevel<=redId) {
       digitalWrite(red, HIGH);
     }
-    
-    wtv020sd16p.playVoice(0);
-    wtv020sd16p.asyncPlayVoice(1);
-    delay(40000); // This indicates how long the song will play for 
-    wtv020sd16p.pauseVoice();
-    delay(2000); 
-    wtv020sd16p.asyncPlayVoice(1);
-    delay(40000);
-    //wtv020sd16p.asyncPlayVoice(2);
-    //delay(5000);
-    wtv020sd16p.stopVoice();
-    
+//    playSound();
+ 
+    // TODO: set state back to 0 (idle)
+  } else {
     digitalWrite(blue, LOW);
     digitalWrite(green, LOW);
     digitalWrite(yellow, LOW);
-    digitalWrite(red, LOW);
-    // TODO: set state back to 0 (idle)
+    digitalWrite(red, LOW); 
   }
   delay(3000);
 }
+
+int getTrashLevel() {
+  int distance = Dist.getDistanceCentimeter();
+  distance = map(distance, 0, maxDistance, 0, 3);
+  return distance;
+}
+
+void playSound() {
+  wtv020sd16p.playVoice(0);
+  wtv020sd16p.asyncPlayVoice(1);
+  delay(40000); // This indicates how long the song will play for 
+  wtv020sd16p.pauseVoice();
+  delay(2000); 
+  wtv020sd16p.asyncPlayVoice(1);
+  delay(40000);
+  //wtv020sd16p.asyncPlayVoice(2);
+  //delay(5000);
+  wtv020sd16p.stopVoice();  
+}
+
 
